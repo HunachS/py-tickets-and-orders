9d57292040089cd4444703d6c395e8a5ef5e2bf8
Hunach <sasha2001sek@gmail.com>
2026-01-15 13:32:57 +0200

    fix_many_details

--- services/order.py
+++ services/order.py
@@ -2,31 +2,32 @@ from typing import List, Optional
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from db.models import Order, Ticket
+from django.db.models import QuerySet
 
 
+@transaction.atomic
 def create_order(tickets: List[dict],
                  username: str, date: Optional[str] = None) -> None:
     user = get_user_model()
 
-    with transaction.atomic():
-        user = user.objects.get(username=username)
+    user = user.objects.get(username=username)
 
-        order = Order.objects.create(user=user)
+    order = Order.objects.create(user=user)
 
-        if date:
-            order.created_at = date
-            order.save()
+    if date:
+        Order.objects.filter(id=order.id).update(created_at=date)
+        order.refresh_from_db()
 
-        for ticket_data in tickets:
-            Ticket.objects.create(
-                row=ticket_data["row"],
-                seat=ticket_data["seat"],
-                movie_session_id=ticket_data["movie_session"],
-                order=order
-            )
+    for ticket_data in tickets:
+        Ticket.objects.create(
+            row=ticket_data["row"],
+            seat=ticket_data["seat"],
+            movie_session_id=ticket_data["movie_session"],
+            order=order
+        )
 
 
-def get_orders(username: Optional[str] = None) -> Order:
+def get_orders(username: Optional[str] = None) -> QuerySet[Order]:
     if username:
         return Order.objects.filter(user__username=username)
     return Order.objects.all()
--- services/user.py
+++ services/user.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-def create_user(username: str, password: int,
+def create_user(username: str, password: str,
                 email: str = None, first_name: str = None,
                 last_name: str = None) -> User:
     user = User.objects.create_user(
@@ -18,10 +18,10 @@ def create_user(username: str, password: int,
 
 
 def get_user(user_id: int) -> User:
-    return User.objects.filter(id=user_id).first()
+    return User.objects.get(id=user_id)
 
 
-def update_user(user_id: int, username: str = None, password: int = None,
+def update_user(user_id: int, username: str = None, password: str = None,
                 email: str = None, first_name: str = None,
                 last_name: str = None) -> User:
     user = User.objects.get(id=user_id)
