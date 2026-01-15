b7603962c69eec4e072ff18b08df34aa2c6ca9b3
Hunach <sasha2001sek@gmail.com>
2026-01-15 13:53:17 +0200

    final

--- db/models.py
+++ db/models.py
@@ -70,7 +70,7 @@ class Order(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self) -> str:
-        return f"<Order: {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}>"
+        return f"{self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
 
 
 class Ticket(models.Model):
@@ -84,9 +84,9 @@ class Ticket(models.Model):
     seat = models.IntegerField()
 
     def __str__(self) -> str:
-        return (f"<Ticket: {self.movie_session.movie.title} "
+        return (f"{self.movie_session.movie.title} "
                 f"{self.movie_session.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
-                f" (row: {self.row}, seat: {self.seat})>")
+                f" (row: {self.row}, seat: {self.seat})")
 
     def clean(self) -> None:
         cinema_hall = self.movie_session.cinema_hall
