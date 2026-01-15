4bbb219de059494e801f1accff6911542fe88eff
Hunach <sasha2001sek@gmail.com>
2026-01-15 13:46:48 +0200

    fix_

--- db/models.py
+++ db/models.py
@@ -88,7 +88,6 @@ class Ticket(models.Model):
                 f"{self.movie_session.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
                 f" (row: {self.row}, seat: {self.seat})>")
 
-
     def clean(self) -> None:
         cinema_hall = self.movie_session.cinema_hall
         if not (1 <= self.row <= cinema_hall.rows):
